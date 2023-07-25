88750734a6093484c56869dfcc817d1bcb21b60f
ESTEB4NGONZ4LEZ <kyosamacps@gmail.com>
2023-07-25 09:07:37 -0500

    Metodo de carga automatica de las configuraciones

--- Infrastructure/Data/TiendaAppContext.cs
+++ Infrastructure/Data/TiendaAppContext.cs
@@ -9,4 +9,11 @@ public class TiendaAppContext : DbContext
     public TiendaAppContext(DbContextOptions<TiendaAppContext> options) : base(options)
     {
     }
+    // * ----- Carga automatica de las Configuraciones -----
+    /* Este metodo asegura que todas las configuraciones definidas en la clase de configuracion que heredan de IEntityTypeConfiguration se apliquen cuando se construye la DB */
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
