77d27f4d13ffb1a7103d6489a4bac39c1cefb671
ESTEB4NGONZ4LEZ <kyosamacps@gmail.com>
2023-07-25 09:21:13 -0500

    Inyeccion de Dependencias

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,18 @@
+using Infrastructure.Datal;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// * ----- Inyeccion de Dependencias -----
+// Este metodo configura un servicio de DbContext y establece que se usara MySQL como el proveedor de DB
+builder.Services.AddDbContext<TiendaAppContext>(optionsBuilder =>
+{
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
