88bfc28248b1541a5f77b1ee069cc7b9fe212104
ESTEB4NGONZ4LEZ <kyosamacps@gmail.com>
2023-07-25 23:24:22 -0500

    DbSet de las Clases

--- Infrastructure/Data/TiendaAppContext.cs
+++ Infrastructure/Data/TiendaAppContext.cs
@@ -1,5 +1,6 @@
 
 using System.Reflection;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Datal;
@@ -9,6 +10,13 @@ public class TiendaAppContext : DbContext
     public TiendaAppContext(DbContextOptions<TiendaAppContext> options) : base(options)
     {
     }
+    // * ----- DbSet ------
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Cliente>? Clientes { get; set; }
+    public DbSet<Factura>? Facturas { get; set; }
+    public DbSet<Producto>? Productos { get; set; }
+    public DbSet<Proveedor>? Proveedores { get; set; }
+    public DbSet<Venta>? Ventas { get; set; }
     // * ----- Carga automatica de las Configuraciones -----
     /* Este metodo asegura que todas las configuraciones definidas en la clase de configuracion que heredan de IEntityTypeConfiguration se apliquen cuando se construye la DB */
     protected override void OnModelCreating(ModelBuilder modelBuilder)
